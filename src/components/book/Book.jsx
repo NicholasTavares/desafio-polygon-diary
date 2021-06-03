4da2638c5ae2db3d8b62cb777914f538e255b53f
Colais <nicholasbalby@hotmail.com>
2021-06-02 21:58:06 -0300

    Iniciando projeto, componetizando e configurando SASS

---
+++ src/components/book/Book.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = () => {
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+export default Book
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Book from '../book/Book'
+
+const Container = () => {
+
+    return(
+        <div className='container'>
+            <Book/>
+        </div>
+    )
+}
+
+export default Container
