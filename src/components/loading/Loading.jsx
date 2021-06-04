2f4077a2cd41fda8c5b706a31e548684cd08361e
Colais <nicholasbalby@hotmail.com>
2021-06-04 09:32:47 -0300

    Animaçao de carregamento concluida.

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loading = () => {
+
+    return (
+        <tr className='table-container__body__row'>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+            <td className='table-container__body__td'><div className='loading'></div></td>
+        </tr>
+    )
+}
+
+export default Loading
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Data from './Data'
 import Title from './Title'
 import Ticker from '../../api/polygon-ticker.js'
+import Loading from '../loading/Loading'
 
 const Table = () => {
     const URL = 'https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=z763xgz4zplhCz0dglQZtc59e9wKGzgM'
@@ -15,7 +16,8 @@ const Table = () => {
         <table className='table-container'>
             <Title />
             <tbody className='table-container__body'>
-                {data ? data.map((data) => (<Data country={data.locale} ticker={data.ticker} name={data.name} />)) : 'Loading...'}
+                {data ? data.map((data) => (<Data country={data.locale} ticker={data.ticker} name={data.name} />)) :
+                    <Loading />}
             </tbody>
         </table>
     )
